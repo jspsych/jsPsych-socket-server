d1df2767267daaeff91d703fedb9868141a3ca4a
Josh de Leeuw <josh.deleeuw@gmail.com>
2018-12-12 14:36:08 -0500

    some work on client library

--- client/jspsych-node.js
+++ client/jspsych-node.js
@@ -1,11 +1,24 @@
-jsPsych.node = (function(){
+var io = require('socket.io-client'); // right way to import this?
 
-  var module = {};
+/* functions here for each kind of message */
+// this is just going to be a bunch of very simple wrappers to make the
+// interface standardized. could possible implement await versions of these too?
 
-  module.connect = function(url){
-    module.socket = io.connect(url);
-  }
+//  stores the client information
+var client;
+var session_id = null;
 
-  return module;
+/*** messages ***/
 
-})();
+export const connect = function(server_url){
+    client = io.connect(server_url);
+}
+
+export const join = function(experiment_id, n_participants){
+    client.on('join-reply', function(data){
+        session_id = data.session_id;
+    });
+    client.emit('join', {experiment_id: experiment_id, participants: n_participants});
+}
+
+
